706b581a8a765d18178e57f9b99a6b7557ed8d80
Simisola Shodunke <simisolashodunke@simisolas-mbp.mynet>
2022-02-28 21:14:05 +0000

    test

---
+++ Excercise 9/say_my_name_2.py
@@ -0,0 +1,6 @@
+first_name = "Simi "
+last_name = "Shodunke"
+
+print(first_name + last_name)
+
+print (list (first_name))
---
+++ Excercise 9/traffic_lights.py
@@ -0,0 +1,72 @@
+# light = input('What colour do you still on the traffic light? ')
+#
+# if light  == 'green':
+#     print('go')
+#     print('That means you can go')
+# elif light == 'red':
+#     print('stop')
+#     print('That means you should stop because the light is not green')
+# elif light == 'amber':
+#     print('Its going to change soon')
+#     print('hurry up')
+# else:
+#     print('thats a weird colour')
+#     print('rather stop')
+#
+# emptyString = ''
+# if emptyString
+#     print("yes")
+# else:
+#     print('its an empty string')
+#
+# zerovalue = 0
+# if zerovalue:
+#     print(0)
+# else:
+#     print('its evaluated to false')
+#
+# a = 4
+# b = 5
+# #not equal
+# if a != b:
+#     print('they are not equal')
+#
+# if light=='green' or light'amber':
+#     print('go')
+#
+# weather = 'rainy'
+# boots = 'yes'
+#
+# if weather=='rainy' and boots=='yes':
+#     print('you may leave the house')
+#
+# locked = False
+#
+# if not locked:
+#     print('lock the door!')
+# else:
+#     print('the door already locked')
+
+myList =['apples', 'orange,'', 'bananas']
+if not all(myList)
+         print('you forgot something')
+
+for element in myList
+    print(element)
+
+myNumList = [4, 5, 8, 2, -9, 22345]
+for number in myNumList:
+    print(number + 7)
+
+# num1 = float(input("Type in your first number:"))
+# num2 = float(input("Type in your second number:"))
+# operator = input("Type in your operator: ")
+# if operator == '+':
+#     print(num1+num2)
+# elif operator == '-':
+#     print(num1-num2)
+
+# num = 0
+#
+# while num != 7:
+#     num = input("Type in a number: ")
